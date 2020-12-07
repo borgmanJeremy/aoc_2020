7518a2fd6e147f7996fb02397c7e709ccc022531
Jeremy Borgman <borgman.jeremy@pm.me>
2020-12-07 12:55:29 -0600

    not pretty but done

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,6 +1,13 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::collections::HashSet;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct BagWithCount {
+    parent: String,
+    child: HashMap<String, i32>,
+}
 
 #[derive(Debug)]
 struct Bag {
@@ -21,7 +28,7 @@ fn find_parents(bag_list: &Vec<Bag>, input: &str) -> Vec<String> {
     parent_list
 }
 
-fn part_1(path: &str) {
+fn parse_bag_list(path: &str) -> Vec<Vec<String>> {
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
 
@@ -39,7 +46,11 @@ fn part_1(path: &str) {
                 .collect()
         })
         .collect();
+    return lines;
+}
 
+fn part_1(path: &str) {
+    let lines = parse_bag_list(path);
     let mut bag_list = Vec::new();
     for tokens in &lines {
         let mut bag = Bag {
@@ -58,9 +69,7 @@ fn part_1(path: &str) {
 
 
     let mut parent_list = HashSet::new();
-
     parent_list.insert(String::from("shiny gold"));
-
 
     let mut found_new = true;
     while found_new {
@@ -81,10 +90,69 @@ fn part_1(path: &str) {
             }
         }
     }
+    println!("{:?}", parent_list.len() - 1);
+}
 
-    println!("{:?}", parent_list.len()-1);
+fn get_children(parent: String, list: &Vec<BagWithCount>) -> Vec<HashMap<String, i32>> {
+    let mut child_list = Vec::new();
+    let mut found = false;
+
+    for bag in list
+    {
+        if bag.parent == parent {
+            child_list.push(bag.child.clone());
+            found = true;
+        }
+    }
+
+    child_list
+}
+
+fn walk_values(initial_value: String, count: i32,  sum: &mut i32, list: &Vec<BagWithCount>){
+    let value = get_children(initial_value, &list);
+    let mut total = 0;
+    for map in value {
+        if !map.is_empty() {
+            for (key, value) in map.into_iter() {
+                total += value*count;
+                *sum =(value*count + *sum);
+                walk_values(key.clone(), count * value, sum, &list);
+                println!("({}, {}), {}", key, value,count*value );
+            }
+        }
+        else {
+        }
+
+    }
+
+}
+
+fn part_2(path: &str) {
+    let lines = parse_bag_list(path);
+    let mut bag_list = Vec::new();
+    for tokens in &lines {
+        let mut bag = BagWithCount {
+            parent: String::from(&tokens[0]),
+            child: HashMap::new(),
+        };
+
+        let child_list: Vec<&str> = tokens[1].split(",")
+            .map(|x| { x.trim() })
+            .collect();
+        for child in child_list {
+            if child != "no other" {
+                bag.child.insert(String::from(&child[2..child.len()]), (child.as_bytes()[0] - 0x30) as i32);
+            }
+        }
+        bag_list.push(bag);
+    }
+
+    let mut total = 0;
+     walk_values(String::from("shiny gold"), 1,&mut total, &bag_list);
+    println!("{:?}", total);
 }
 
 fn main() {
     part_1("/home/jeremy/github/aoc_2020/day_7/input/input_1.txt");
+    part_2("/home/jeremy/github/aoc_2020/day_7/input/input_1.txt");
 }
