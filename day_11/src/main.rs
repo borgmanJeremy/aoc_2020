e524f905a389b0eec6da83e05c1909bd8ca116fc
Jeremy Borgman <borgman.jeremy@pm.me>
2020-12-11 19:31:13 -0600

    Part 2 working

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -10,6 +10,12 @@ struct Coordinate {
     y: usize,
 }
 
+#[derive(Debug, Clone, Copy)]
+enum SearchType {
+    Adjacent,
+    LineOfSight,
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum SeatType {
     Floor,
@@ -78,7 +84,7 @@ fn count_adjacent(seat_map: &Vec<Vec<SeatType>>, seat: &Coordinate) -> (u32, u32
     let mut full_count = 0;
 
     for direction in Direction::iter() {
-        match check_direction(&seat_map, &seat, direction) {
+        match check_near_direction(&seat_map, &seat, direction) {
             SeatType::Floor => { empty_count += 1; }
             SeatType::Empty => { empty_count += 1; }
             SeatType::Full => { full_count += 1; }
@@ -87,60 +93,117 @@ fn count_adjacent(seat_map: &Vec<Vec<SeatType>>, seat: &Coordinate) -> (u32, u32
     return (empty_count, full_count);
 }
 
-fn check_direction(seat_map: &Vec<Vec<SeatType>>, seat: &Coordinate, dir: Direction) -> SeatType {
+fn count_line_of_sight(seat_map: &Vec<Vec<SeatType>>, seat: &Coordinate) -> (u32, u32) {
+    let mut empty_count = 0;
+    let mut full_count = 0;
+
+    for direction in Direction::iter() {
+        let mut temp_seat = *seat;
+        loop {
+            let seat = check_near_direction(&seat_map, &temp_seat, direction);
+            match seat {
+                SeatType::Floor => {
+                    match direction {
+                        Direction::North => {
+                            temp_seat.y -= 1;
+                        }
+                        Direction::NorthEast => {
+                            temp_seat.y -= 1;
+                            temp_seat.x += 1;
+                        }
+                        Direction::East => {
+                            temp_seat.x += 1;
+                        }
+                        Direction::SouthEast => {
+                            temp_seat.y += 1;
+                            temp_seat.x += 1;
+                        }
+                        Direction::South => {
+                            temp_seat.y += 1;
+                        }
+                        Direction::SouthWest => {
+                            temp_seat.y += 1;
+                            temp_seat.x -= 1;
+                        }
+                        Direction::West => {
+                            temp_seat.x -= 1;
+                        }
+                        Direction::NorthWest => {
+                            temp_seat.y -= 1;
+                            temp_seat.x -= 1;
+                        }
+                    }
+                }
+                SeatType::Empty => {
+                    empty_count += 1;
+                    break;
+                }
+                SeatType::Full => {
+                    full_count += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    return (empty_count, full_count);
+}
+
+
+fn check_near_direction(seat_map: &Vec<Vec<SeatType>>, seat: &Coordinate, dir: Direction) -> SeatType {
     match dir {
         Direction::North => {
             if seat.y == 0 {
-                SeatType::Floor
+                SeatType::Empty
             } else {
-                seat_map[seat.y-1][seat.x]
+                seat_map[seat.y - 1][seat.x]
             }
         }
         Direction::NorthEast => {
             if seat.y == 0 || seat.x >= seat_map[0].len() - 1 {
-                SeatType::Floor
+                SeatType::Empty
             } else {
                 seat_map[seat.y - 1][seat.x + 1]
             }
         }
         Direction::East => {
             if seat.x >= seat_map[0].len() - 1 {
-                SeatType::Floor
+                SeatType::Empty
             } else {
-                seat_map[seat.y][seat.x +1]
+                seat_map[seat.y][seat.x + 1]
             }
         }
         Direction::SouthEast => {
             if seat.x >= seat_map[0].len() - 1 || seat.y >= seat_map.len() - 1 {
-                SeatType::Floor
+                SeatType::Empty
             } else {
-                seat_map[seat.y + 1][seat.x+ 1]
+                seat_map[seat.y + 1][seat.x + 1]
             }
         }
         Direction::South => {
             if seat.y >= seat_map.len() - 1 {
-                SeatType::Floor
+                SeatType::Empty
             } else {
                 seat_map[seat.y + 1][seat.x]
             }
         }
         Direction::SouthWest => {
             if seat.y >= seat_map.len() - 1 || seat.x == 0 {
-                SeatType::Floor
+                SeatType::Empty
             } else {
                 seat_map[seat.y + 1][seat.x - 1]
             }
         }
         Direction::West => {
             if seat.x == 0 {
-                SeatType::Floor
+                SeatType::Empty
             } else {
                 seat_map[seat.y][seat.x - 1]
             }
         }
         Direction::NorthWest => {
             if seat.x == 0 || seat.y == 0 {
-                SeatType::Floor
+                SeatType::Empty
             } else {
                 seat_map[seat.y - 1][seat.x - 1]
             }
@@ -148,17 +211,25 @@ fn check_direction(seat_map: &Vec<Vec<SeatType>>, seat: &Coordinate, dir: Direct
     }
 }
 
-fn update_map(seat_map: &Vec<Vec<SeatType>>) -> Vec<Vec<SeatType>> {
+fn update_map(seat_map: &Vec<Vec<SeatType>>, threshold: u32, search_type: SearchType) -> Vec<Vec<SeatType>> {
     let mut new_map = Vec::new();
     for (y, row) in seat_map.iter().enumerate() {
         let mut new_row = Vec::new();
         for (x, _seat) in row.iter().enumerate() {
-            let count = count_adjacent(seat_map, &Coordinate { x, y });
+            let count = match search_type {
+                SearchType::Adjacent => {
+                    count_adjacent(seat_map, &Coordinate { x, y })
+                }
+                SearchType::LineOfSight => {
+                    count_line_of_sight(seat_map, &Coordinate { x, y })
+                }
+            };
+
             if seat_map[y][x] == SeatType::Floor {
                 new_row.push(SeatType::Floor);
             } else if seat_map[y][x] == SeatType::Empty && (count.0 == 8) {
                 new_row.push(SeatType::Full);
-            } else if count.1 >= 4 {
+            } else if count.1 >= threshold {
                 new_row.push(SeatType::Empty);
             } else {
                 new_row.push(seat_map[y][x])
@@ -167,6 +238,68 @@ fn update_map(seat_map: &Vec<Vec<SeatType>>) -> Vec<Vec<SeatType>> {
         new_map.push(new_row);
     }
     new_map
+}
+
+
+fn part_1() {
+    let mut orig_map = parse_file("/home/jeremy/github/aoc_2020/day_11/input/input_1.txt");
+    //print_map(&orig_map);
+
+    loop {
+        let new_map = update_map(&orig_map, 4, SearchType::Adjacent);
+        //println!("");
+        //println!("");
+        //println!("");
+        //print_map(&new_map);
+        if new_map == orig_map {
+            break;
+        }
+        orig_map = new_map;
+    }
+
+    let mut full_count = 0;
+    for (y, row) in orig_map.iter().enumerate() {
+        for (x, _seat) in row.iter().enumerate() {
+            if orig_map[y][x] == SeatType::Full {
+                full_count += 1;
+            }
+        }
+    }
+
+    println!("Full Count: {}", full_count);
+}
+
+
+fn part_2() {
+    let mut orig_map = parse_file("/home/jeremy/github/aoc_2020/day_11/input/input_1.txt");
+    //print_map(&orig_map);
+
+    loop {
+        let new_map = update_map(&orig_map, 5, SearchType::LineOfSight);
+        //println!("");
+        //println!("");
+        //println!("");
+        //print_map(&new_map);
+        if new_map == orig_map {
+            break;
+        }
+        orig_map = new_map;
+    }
+
+    let mut full_count = 0;
+    for (y, row) in orig_map.iter().enumerate() {
+        for (x, _seat) in row.iter().enumerate() {
+            if orig_map[y][x] == SeatType::Full {
+                full_count += 1;
+            }
+        }
+    }
+    println!("Full Count: {}", full_count);
+}
+
+fn main() {
+    part_1();
+    part_2();
 }
 
 
@@ -181,26 +314,24 @@ mod tests {
         map.push(vec![SeatType::Full, SeatType::Full, SeatType::Full]);
         map.push(vec![SeatType::Full, SeatType::Full, SeatType::Full]);
 
-        assert_eq!(check_direction(&map, &Coordinate { x: 0, y: 0 }, Direction::North), SeatType::Floor);
-        assert_eq!(check_direction(&map, &Coordinate { x: 2, y: 0 }, Direction::NorthEast), SeatType::Floor);
-        assert_eq!(check_direction(&map, &Coordinate { x: 2, y: 0 }, Direction::East), SeatType::Floor);
-        assert_eq!(check_direction(&map, &Coordinate { x: 2, y: 2 }, Direction::SouthEast), SeatType::Floor);
-        assert_eq!(check_direction(&map, &Coordinate { x: 0, y: 2 }, Direction::South), SeatType::Floor);
-        assert_eq!(check_direction(&map, &Coordinate { x: 0, y: 2 }, Direction::SouthWest), SeatType::Floor);
-        assert_eq!(check_direction(&map, &Coordinate { x: 0, y: 0 }, Direction::West), SeatType::Floor);
-        assert_eq!(check_direction(&map, &Coordinate { x: 0, y: 0 }, Direction::NorthWest), SeatType::Floor);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 0, y: 0 }, Direction::North), SeatType::Empty);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 2, y: 0 }, Direction::NorthEast), SeatType::Empty);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 2, y: 0 }, Direction::East), SeatType::Empty);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 2, y: 2 }, Direction::SouthEast), SeatType::Empty);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 0, y: 2 }, Direction::South), SeatType::Empty);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 0, y: 2 }, Direction::SouthWest), SeatType::Empty);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 0, y: 0 }, Direction::West), SeatType::Empty);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 0, y: 0 }, Direction::NorthWest), SeatType::Empty);
 
         map = Vec::new();
-        map.push(vec![SeatType::Full, SeatType::Full, SeatType::Floor]);
+        map.push(vec![SeatType::Full, SeatType::Full, SeatType::Empty]);
         map.push(vec![SeatType::Full, SeatType::Full, SeatType::Full]);
         map.push(vec![SeatType::Full, SeatType::Full, SeatType::Full]);
 
-        assert_eq!(check_direction(&map, &Coordinate { x: 1, y: 0 }, Direction::North), SeatType::Floor);
-        assert_eq!(check_direction(&map, &Coordinate { x: 1, y: 0 }, Direction::West), SeatType::Full);
-        assert_eq!(check_direction(&map, &Coordinate { x: 1, y: 0 }, Direction::SouthWest), SeatType::Full);
-        assert_eq!(check_direction(&map, &Coordinate { x: 1, y: 0 }, Direction::South), SeatType::Full);
-
-
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 1, y: 0 }, Direction::North), SeatType::Empty);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 1, y: 0 }, Direction::West), SeatType::Full);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 1, y: 0 }, Direction::SouthWest), SeatType::Full);
+        assert_eq!(check_near_direction(&map, &Coordinate { x: 1, y: 0 }, Direction::South), SeatType::Full);
     }
 
     #[test]
@@ -221,41 +352,3 @@ mod tests {
         assert_eq!(count_adjacent(&map, &Coordinate { x: 1, y: 0 }), (4, 4));
     }
 }
-
-fn main() {
-    let mut orig_map = parse_file("/home/jeremy/github/aoc_2020/day_11/input/input_1.txt");
-    print_map(&orig_map);
-
-
-    loop {
-        let new_map = update_map(&orig_map);
-        println!("");
-        println!("");
-        println!("");
-        print_map(&new_map);
-        if new_map == orig_map{
-            break;
-        }
-        orig_map = new_map;
-    }
-
-    let mut full_count = 0;
-    for (y, row) in orig_map.iter().enumerate(){
-        for (x, _seat) in row.iter().enumerate(){
-            if orig_map[y][x]==SeatType::Full{
-                full_count +=1;
-            }
-        }
-    }
-
-    println!("Full Count: {}", full_count);
-}
-
-/*
-
-    If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
-    If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
-    Otherwise, the seat's state does not change.
-
-Floor (.) never changes; seats don't move, and nobody sits on the floor.
- */
